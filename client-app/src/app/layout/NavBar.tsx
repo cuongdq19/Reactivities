2d0a5500bfe02c380de3824ad31c1a2856294130
Cuong Dang <cuong.developer19@gmail.com>
2021-06-19 18:23:13 +0700

    End of section 17

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -36,7 +36,7 @@ const NavBar = () => {
             <Dropdown.Menu>
               <Dropdown.Item
                 as={Link}
-                to={`/profile/${user?.username}`}
+                to={`/profiles/${user?.username}`}
                 text="My Profile"
                 icon="user"
               />
